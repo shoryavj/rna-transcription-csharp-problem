624ec5568c05f8b000441c729ae638619bf5c0f2
shorya.vijayvargiya <shorya.vijayvargiya@cgi.com>
2019-08-29 15:34:30 +0530

    hi

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
-namespace RNATranscriptionModule
+
+namespace RNATranscriptionModule{
+
+
+public static class RnaTranscription
 {
-    public static class RnaTranscription
+     
+    static void Main(string[] args)
     {
-        public static string ToRna(string nucleotide)
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        
     }
+
+        static readonly IReadOnlyDictionary<char, char> TranslateTable = new Dictionary<char, char>
+  {
+      ['G'] = 'C',
+      ['C'] = 'G',
+      ['T'] = 'A',
+      ['A'] = 'U',
+  };
+           
+       public static string ToRna(string nucleotide) 
+        {
+
+            StringBuilder sb = new StringBuilder(nucleotide) ;
+            for(int i=0; i< sb.Length; i++) {
+                sb[i] = TranslateTable[sb[i]]  ;
+            }
+            return sb.ToString();
+        }
 }
+                                }
